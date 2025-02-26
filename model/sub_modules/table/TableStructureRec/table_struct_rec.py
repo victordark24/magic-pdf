846a935d8da0464eecdc21e3d226e326aca67bda
Victor <605226746@qq.com>
2025-02-26 15:41:54 +0800

    1
    1

---
+++ model/sub_modules/table/TableStructureRec/table_struct_rec.py
@@ -0,0 +1,37 @@
+
+import os
+import cv2
+import numpy as np
+from pathlib import Path
+from lineless_table_rec import LinelessTableRecognition
+from lineless_table_rec.utils_table_recover import format_html, plot_rec_box_with_logic_info, plot_rec_box
+from table_cls import TableCls
+from wired_table_rec import WiredTableRecognition
+from PIL import Image
+
+class TableStructRec():
+    def __init__(self):
+        self.lineless_engine = LinelessTableRecognition()
+        self.wired_engine = WiredTableRecognition()
+        self.table_cls = TableCls()
+
+    def predict(self, image):
+        self.lineless_engine = LinelessTableRecognition()
+        self.wired_engine = WiredTableRecognition()
+        self.table_cls = TableCls()
+
+        # image.save("save.jpg")
+        image = cv2.cvtColor(np.asarray(image), cv2.COLOR_RGB2BGR)
+        cls, elasp = self.table_cls(image)
+        if cls == 'wired':
+            table_engine = self.wired_engine
+        else:
+            table_engine = self.lineless_engine
+
+        html, elasp, polygons, logic_points, ocr_res = table_engine(image)
+        # complete_html = format_html(html)
+        return html, None, logic_points, elasp
+        # return complete_html, None, logic_points, elasp
+
+
+    #        return html_code, table_cell_bboxes, logic_points, elapse
